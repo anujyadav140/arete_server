6b55285e2c53d2d1e831c451a2e85e77adbd5ca3
Drexvo <anujyadav140@gmail.com>
2022-04-17 12:37:28 +0530

    fixed a bug in channels

--- lib/arete_server_web/channels/room_channel.ex
+++ lib/arete_server_web/channels/room_channel.ex
@@ -1,45 +1,35 @@
 defmodule AreteServerWeb.RoomChannel do
   use AreteServerWeb, :channel
-  alias AreteServer.Chat
+  # alias AreteServer.Chat
   alias AreteServer.Chat.Message, as: MessageRepo
 
   @new_message "new_message"
 
-  def join("room" <> room_id, _payload, socket) do
-    # IO.inspect(room_id)
-    # IO.inspect(payload)
-    room = Chat.get_room(room_id)
-
-    if room == nil do
-      {:error, socket}
-    else
-      {:ok, assign(socket, :room_id, room_id)}
-    end
+  def join("room:" <> room_id, _payload, socket) do
+    IO.inspect("joining channel")
+    {:ok, assign(socket, :room_id, room_id)}
   end
 
-  def handle_in(@new_message, %{"payload" => %{"content" => content}}, socket) do
-    # IO.inspect("payload of #{@new_message}")
-    # IO.inspect(content)
-    # IO.inspect('assigns the room id:')
-    # IO.inspect(socket.assigns.room_id)
+  def handle_in("new_message", %{"payload" => %{"content" => content}}, socket) do
+    res = MessageRepo.create_message(%{room_id: socket.assigns.room_id, content: content})
 
-    room_id = socket.assigns.room_id
-
-    case MessageRepo.create_message(%{content: content, room_id: room_id}) do
-      {:ok, _message} ->
-        # IO.inspect("new message!!!")
-        broadcast!(socket, @new_message, %{payload: %{content: content}})
+    case res do
+      {:ok, new_message} ->
+        IO.inspect("broadcasting new message....")
+        broadcast!(socket, @new_message, %{data: new_message})
         {:noreply, socket}
 
       {:error, %Ecto.Changeset{} = _changeset} ->
-        {:noreply, socket}
+        {:error, socket}
 
-      true ->
-        {:noreply, socket}
+      _ ->
+        {:error, socket}
     end
   end
 
-  def handle_in(@new_message, _payload, socket) do
+  def handle_in("new_message", payload, socket) do
+    IO.inspect("payload done")
+    IO.inspect(payload)
     {:noreply, socket}
   end
 end
